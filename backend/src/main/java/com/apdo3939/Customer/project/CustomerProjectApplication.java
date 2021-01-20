bbfc1b7c126e30b0b47d7f1b1a8f85dcecb285d9
Apdo3939 <apdo3939@hotmail.com>
2021-01-20 16:37:01 -0300

    Created Project

---
+++ backend/src/main/java/com/apdo3939/Customer/project/CustomerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.apdo3939.Customer.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/apdo3939/Customer/project/CustomerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apdo3939.Customer.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
